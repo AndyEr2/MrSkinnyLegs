e29601e2283ba5a4d23159d4f9c23c5c334f4d4e
Andriy Dragan <andriy.dragan@3shape.com>
2020-04-18 13:05:58 +0300

    Base functionality implemented

--- WebSpider/Parser.cs
+++ WebSpider/Parser.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
 
 namespace WebSpider
 {
-    public class Parser
+    public static class Parser
     {
+        public static async System.Threading.Tasks.Task<string> GetTextAsync(string webAdress)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(webAdress);
+            var pageContents = await response.Content.ReadAsStringAsync();
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            return pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@class,'desc-text')])[1]").InnerText;
+        }
     }
 }
